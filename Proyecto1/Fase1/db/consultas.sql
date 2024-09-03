a604fa88826487075061966c4b85ebf1b1f69156
Iskandar1412 <silvapaco810@gmail.com>
2024-09-03 00:32:35 -0600

    [UPDATE]: Modificacion Esquema

--- Proyecto1/Fase1/db/consultas.sql
+++ Proyecto1/Fase1/db/consultas.sql
@@ -1,2 +1,15 @@
 SELECT COUNT(*) AS Total_Compras FROM FactCompras;
 SELECT COUNT(*) AS Total_Ventas FROM FactVentas;
+
+DELETE FROM Compras;
+DELETE FROM Ventas;
+DELETE FROM Sucursal;
+DELETE FROM Producto;
+DELETE FROM Region;
+DELETE FROM Departamento;
+DELETE FROM Marca;
+DELETE FROM Categoria;
+DELETE FROM Fecha;
+DELETE FROM Proveedor;
+DELETE FROM Cliente;
+DELETE FROM Vendedor;
--- Proyecto1/Fase1/db/dw.sql
+++ Proyecto1/Fase1/db/dw.sql
@@ -1,61 +1,85 @@
-CREATE DATABASE DB3Seminario2_201906051;
+CREATE DATABASE DWH_Seminario2_201906051;
 
 CREATE TABLE Fecha(
-	fecha_key int IDENTITY(1,1) PRIMARY KEY,
-	year int,
-    month int,
-    day int
+	fecha_key INT IDENTITY(1,1) PRIMARY KEY,
+	year INT,
+    month INT,
+    day INT
 );
 
 CREATE TABLE Proveedor(
-	proveedor_key int IDENTITY(1,1) PRIMARY KEY,
-	CodProveedor varchar(100),
-	NombreProveedor varchar(100),
-	DireccionProveedor varchar(255),
-	NumeroProveedor int,
-	WebProveedor varchar(100)
+	proveedor_key INT IDENTITY(1,1) PRIMARY KEY,
+	CodProveedor NVARCHAR(100),
+	NombreProveedor NVARCHAR(100),
+	DireccionProveedor NVARCHAR(255),
+	NumeroProveedor INT,
+	WebProveedor NVARCHAR(100)
 );
 
 CREATE TABLE Cliente(
-	cliente_key int IDENTITY(1,1) PRIMARY KEY,
-	CodigoCliente varchar(100),
-	NombreCliente varchar(100),
-	TipoCliente varchar(100),
-	DireccionCliente varchar(255),
-	NumeroCliente int
+	cliente_key INT IDENTITY(1,1) PRIMARY KEY,
+	CodigoCliente NVARCHAR(100),
+	NombreCliente NVARCHAR(100),
+	TipoCliente NVARCHAR(100),
+	DireccionCliente NVARCHAR(255),
+	NumeroCliente INT
 );
 
 CREATE TABLE Vendedor(
-	vendedor_key int IDENTITY(1,1) PRIMARY KEY,
-	CodVendedor varchar(100),
-	NombreVendedor varchar(100),
-	Vacacionista bit
+	vendedor_key INT IDENTITY(1,1) PRIMARY KEY,
+	CodVendedor NVARCHAR(100),
+	NombreVendedor NVARCHAR(100),
+	Vacacionista BIT
+);
+
+CREATE TABLE Marca(
+	marca_key INT IDENTITY(1,1) PRIMARY KEY,
+	MarcaProducto NVARCHAR(100)
+);
+
+CREATE TABLE Categoria(
+	categoria_key INT IDENTITY(1,1) PRIMARY KEY,
+	Categoria NVARCHAR(100)
 );
 
 CREATE TABLE Producto(
-	producto_key int IDENTITY(1,1) PRIMARY KEY,
-	CodProducto varchar(100),
-	NombreProducto varchar(100),
-	MarcaProducto varchar(100),
-	Categoria varchar(100)
+	producto_key INT IDENTITY(1,1) PRIMARY KEY,
+	CodProducto NVARCHAR(100),
+	NombreProducto NVARCHAR(100),
+	marca_key INT,
+	categoria_key INT,
+	FOREIGN KEY (marca_key) REFERENCES Marca(marca_key),
+	FOREIGN KEY (categoria_key) REFERENCES Categoria(categoria_key)
+);
+
+CREATE TABLE Departamento(
+	departamento_key INT IDENTITY(1,1) PRIMARY KEY,
+	Departamento NVARCHAR(100)
+);
+
+CREATE TABLE Region(
+	region_key INT IDENTITY(1,1) PRIMARY KEY,
+	Region NVARCHAR(100),
+	departamento_key INT,
+	FOREIGN KEY (departamento_key) REFERENCES Departamento(departamento_key)
 );
 
 CREATE TABLE Sucursal(
-    sucursal_key int IDENTITY(1,1) PRIMARY KEY,
-	SodSucursal varchar(255),
-	NombreSucursal varchar(100),
-	DireccionSucursal varchar(255),
-	Region varchar(100),
-	Departamento varchar(100)
+	sucursal_key INT IDENTITY(1,1) PRIMARY KEY,
+	SodSuSursal NVARCHAR(100),
+	NombreSucursal NVARCHAR(100),
+	DireccionSucursal NVARCHAR(255),
+	region_key INT,
+	FOREIGN KEY (region_key) REFERENCES Region(region_key)
 );
 
 CREATE TABLE Compras(
-	fecha_key int,
-	proveedor_key int, 
-	producto_key int,
-	sucursal_key int,
-	Unidades int, 
-	CostoU decimal,
+	fecha_key INT,
+	proveedor_key INT, 
+	producto_key INT,
+	sucursal_key INT,
+	Unidades INT, 
+	CostoU DECIMAL,
 	FOREIGN KEY (fecha_key) REFERENCES Fecha(fecha_key),
 	FOREIGN KEY (proveedor_key) REFERENCES Proveedor(proveedor_key),
 	FOREIGN KEY (producto_key) REFERENCES Producto(producto_key),
@@ -63,13 +87,13 @@ CREATE TABLE Compras(
 );
 
 CREATE TABLE Ventas(
-	fecha_key int,
-	cliente_key int, 
-	vendedor_key int, 
-	producto_key int,
-	sucursal_key int,
-	Unidades int, 
-	PrecioUnitario decimal,
+	fecha_key INT,
+	cliente_key INT, 
+	vendedor_key INT, 
+	producto_key INT,
+	sucursal_key INT,
+	Unidades INT, 
+	PrecioUnitario DECIMAL,
 	FOREIGN KEY (fecha_key) REFERENCES Fecha(fecha_key),
 	FOREIGN KEY (cliente_key) REFERENCES Cliente(cliente_key),
 	FOREIGN KEY (vendedor_key) REFERENCES Vendedor(vendedor_key),
